c29dec87f1dd029eea8891815b5da2d0342899fd
machida-yosuke <machida-yosuke@kayac.com>
2024-02-11 00:57:26 +0900

    かくれいいね

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ja">
-      <title>隠れいいね</title>
+      <title>かくれいいね</title>
       <Head />
       <body>
         <Main />
