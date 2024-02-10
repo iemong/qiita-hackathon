8e071d7531016fda6d6f037104f135d5e42afa60
iemong <kiyoshidainagon@gmail.com>
2024-02-10 19:56:23 +0900

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,19 @@
 import { Hono } from "hono";
 import { showRoutes } from "hono/dev";
 import { logger } from "hono/logger";
+import { cors } from "hono/cors";
 import { Bindings } from "./binding";
 import { articlesRoute, registerRoute } from "./api";
 
 const app = new Hono<{ Bindings: Bindings }>();
 
 app.use("*", logger());
+app.use(
+  "/api/*",
+  cors({
+    origin: ["http://localhost:3000", "https://qiita-hackathon.pages.dev"],
+  }),
+);
 
 app.route("/api/register", registerRoute);
 app.route("/api/articles", articlesRoute);
