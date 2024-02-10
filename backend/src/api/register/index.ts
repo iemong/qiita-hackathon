055e1bcd60a8bb7fbe3cc3600a50577a0db5ad4e
iemong <kiyoshidainagon@gmail.com>
2024-02-10 23:34:31 +0900

    Refactor register route to handle multiple URLs in the message

--- backend/src/api/register/index.ts
+++ backend/src/api/register/index.ts
@@ -1,41 +1,50 @@
 import { Hono } from "hono";
+import { drizzle } from "drizzle-orm/d1";
 import { Bindings } from "../../binding";
 import {
   urlsService,
   reactionsService,
   urlReactionsService,
 } from "../../services";
-import { drizzle } from "drizzle-orm/d1";
+import { extractUrls } from "../../utils/extractUrls";
 
 const registerRoute = new Hono<{ Bindings: Bindings }>();
 
 registerRoute.post("/", async (c) => {
   const data: { message: string; reaction: string } = await c.req.json();
-  const { message: url, reaction } = data;
+  const { message, reaction } = data;
 
   const db = drizzle(c.env.DB);
-  const u =
-    (await urlsService.findUrl(db, url)) ??
-    (await urlsService.insertUrl(db, url));
+
   const r =
     (await reactionsService.findReaction(db, reaction)) ??
     (await reactionsService.insertReaction(db, reaction));
 
-  const urlReaction = await urlReactionsService.findUrlReaction(db, {
-    urlId: u.id,
-    reactionId: r.id,
+  const pickUrls = extractUrls(message);
+
+  const asyncUrlRegisters = pickUrls.map(async (url) => {
+    const u =
+      (await urlsService.findUrl(db, url)) ??
+      (await urlsService.insertUrl(db, url));
+
+    const urlReaction = await urlReactionsService.findUrlReaction(db, {
+      urlId: u.id,
+      reactionId: r.id,
+    });
+    if (urlReaction) {
+      await urlReactionsService.incrementUrlReaction(db, {
+        urlId: u.id,
+        reactionId: r.id,
+      });
+    } else {
+      await urlReactionsService.insertUrlReaction(db, {
+        reactionId: r.id,
+        urlId: u.id,
+      });
+    }
   });
-  if (urlReaction) {
-    await urlReactionsService.incrementUrlReaction(db, {
-      urlId: u.id,
-      reactionId: r.id,
-    });
-  } else {
-    await urlReactionsService.insertUrlReaction(db, {
-      reactionId: r.id,
-      urlId: u.id,
-    });
-  }
+
+  await Promise.all(asyncUrlRegisters);
 
   return c.body("ok", 200);
 });
